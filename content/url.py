9f8f598e1735953c6846edfd66f7141adbec5505
iSOLveIT <isolveitgroup@gmail.com>
2020-04-13 16:19:36 +0000

    Connected to mongo database

--- content/url.py
+++ content/url.py
@@ -12,7 +12,7 @@ app.add_url_rule('/about', view_func=AboutEndpoint.as_view("about"))
 app.add_url_rule('/contact', view_func=ContactEndpoint.as_view("contact"))
 
 # Route for blog category
-app.add_url_rule('/category', view_func=CategoryEndpoint.as_view("category"))
+app.add_url_rule('/category/<string:category>/', view_func=CategoryEndpoint.as_view("category"))
 
 # Route for single blog
-app.add_url_rule('/blogpost', view_func=SingleEndpoint.as_view("blogpost"))
+app.add_url_rule('/category/<string:category>/blogpost/<string:blog_id>/', view_func=SingleEndpoint.as_view("blogpost"))
